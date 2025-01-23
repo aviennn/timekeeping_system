4b519667a98b6b527bf6b0a4922575e2449592fa
andreiagbisit <agbisit.andreijoaqhimali@auf.edu.ph>
2025-01-24 02:02:40 +0800

    designed Admin UI

--- timekeeps/settings.py
+++ timekeeps/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'jazzmin',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -122,3 +123,110 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+JAZZMIN_SETTINGS = {
+    "site_title": "Academe TS",
+    "site_header": "Academe TS",
+    "site_brand": "Academe TS",
+    "site_logo":"images/icon-1.png",
+    "login_logo":"images/icon-1.png",
+    "login_logo_dark":"images/icon-2.png",
+    #"site_logo_classes": "",
+    "site_icon":"images/icon-1.png",
+    "welcome_sign": "Academe TS by GOCLOUD",
+    "copyright": "GOCLOUD Asia, Inc",
+    #"search_model": ["auth.User", "auth.Group"],
+    "user_avatar": "",
+
+    # TOP MENU
+
+    "topmenu_links": [
+        #{"name": "Home",  "url": "admin:index", "permissions": ["auth.view_user"]},
+        #{"name": "GOCLOUD Website", "url": "https://gocloudgroup.com/", "new_window": True},
+        #{"model": "auth.User"},
+        #{"app": "TimeKeeping_App"},
+    ],
+
+    # USER MENU
+
+    "usermenu_links": [
+        {"name": "GOCLOUD Website", "url": "https://gocloudgroup.com/", "new_window": True, "icon": "fas fa-globe"},
+        #{"model": "auth.user"}
+    ],
+
+    # SIDE MENU
+
+    #"show_sidebar": True,
+    #"navigation_expanded": True,
+    #"hide_apps": [],
+    #"hide_models": [],
+    #"order_with_respect_to": ["auth", "books", "books.author", "books.book"],
+
+    #"custom_links": {
+        #"books": [{
+            #"name": "Make Messages", 
+            #"url": "make_messages", 
+            #"icon": "fas fa-comments",
+            #"permissions": ["books.view_book"]
+        #}]
+    #},
+
+    "icons": {
+        "auth": "fas fa-users-cog",
+        "auth.user": "fas fa-user",
+        "auth.Group": "fas fa-users",
+        "TimeKeeping_App.employee": "fas fa-user-tie",
+        "TimeKeeping_App.timerecord": "far fa-clock	",
+    },
+
+    #"default_icon_parents": "fas fa-chevron-circle-right",
+    #"default_icon_children": "fas fa-circle",
+
+    # RELATED MODAL
+
+    #"related_modal_active": False,
+
+    # UI TWEAKS
+
+    "custom_css": "style.css",
+    #"custom_js": None,
+    #"use_google_fonts_cdn": True,
+    "show_ui_builder": True,
+
+    # CHANGE VIEW
+    "changeform_format": "horizontal_tabs",
+    #"changeform_format_overrides": {"auth.user": "collapsible", "auth.group": "vertical_tabs"},
+    #"language_chooser": True,
+}
+
+JAZZMIN_UI_TWEAKS = {
+    "navbar_small_text": False,
+    "footer_small_text": False,
+    "body_small_text": False,
+    "brand_small_text": False,
+    "brand_colour": "navbar-dark",
+    "accent": "accent-info",
+    "navbar": "navbar-primary navbar-dark",
+    "no_navbar_border": True,
+    "navbar_fixed": False,
+    "layout_boxed": False,
+    "footer_fixed": False,
+    "sidebar_fixed": False,
+    "sidebar": "sidebar-light-teal",
+    "sidebar_nav_small_text": False,
+    "sidebar_disable_expand": False,
+    "sidebar_nav_child_indent": False,
+    "sidebar_nav_compact_style": False,
+    "sidebar_nav_legacy_style": False,
+    "sidebar_nav_flat_style": True,
+    "theme": "flatly",
+    "dark_mode_theme": None,
+    "button_classes": {
+        "primary": "btn-outline-primary",
+        "secondary": "btn-outline-secondary",
+        "info": "btn-info",
+        "warning": "btn-warning",
+        "danger": "btn-danger",
+        "success": "btn-success"
+    }
+}
