7299ebc91a389637f5fd2041e7a519fd9df90f85
aviennn <avienflairebatul@gmail.com>
2025-01-29 11:24:29 +0800

    settings

--- timekeeps/settings.py
+++ timekeeps/settings.py
@@ -77,11 +77,14 @@ WSGI_APPLICATION = 'timekeeps.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'timekeeping_system', 
+        'USER': 'root',  
+        'PASSWORD': 'ssadwq0010',  
+        'HOST': 'localhost',  
+        'PORT': '3306',  
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
